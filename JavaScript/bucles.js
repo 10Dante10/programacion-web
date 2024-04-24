b69fba4f8ff7ab1fac0964d9408884096c908188
dante fama <danttee.fama@gmail.com>
2024-04-24 15:34:02 -0300

    termine

---
+++ JavaScript/bucles.js
@@ -0,0 +1,11 @@
+const arrays = (1,2,3)
+let contador = 0;
+
+while (contador <2){
+    console.log("hola")
+    contador++;
+}
+
+for(let i = 0;i< arrays,lenght;i++){
+    console.log("array",arrays[1]);
+}
---
+++ JavaScript/variables.js
@@ -0,0 +1,6 @@
+var name = "dante";
+var number = 20;
+var boolean = true;
+function funcion () {
+    let name = "dante";
+}
