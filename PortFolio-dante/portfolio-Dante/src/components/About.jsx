c415abd70c55c1a00eca45000bbf6281deb56831
dante <danttee.fama@gmail.com>
2024-08-28 16:43:26 -0300

    arregle la biografia

--- PortFolio-dante/portfolio-Dante/src/components/About.jsx
+++ PortFolio-dante/portfolio-Dante/src/components/About.jsx
@@ -8,10 +8,11 @@ function About() {
         Dante Nicolás Famá (C.A.B.A, 19 de Diciembre de 2006), conocido como
         Dante, es un programador argentino que programa distantos proyectos en
         el colegio de la etec uba . Actualmente esta en reserva de futsal en
-        Velez, y si dios quiere este finde debuta. ig:_dantteee
+        Velez, citado varias ocasiones a 1ra pero sin oportunidad de minutos.
+        Goles: 15 Asistencias:31 Faltas cometidas:0 Amarillas:1 Rojas:0
       </p>
       <div className="centrar1">
-        <img src="10.jpg" />
+        <img src="DanteMIni.png" />
       </div>
     </div>
   );
